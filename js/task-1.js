39bb06ff83891b3606a4898ad0e773a4519c5989
Nicola <menekse776@gmail.com>
2024-12-04 15:14:41 +0300

    updatehw

--- js/task-1.js
+++ js/task-1.js
@@ -13,6 +13,7 @@ function isEnoughCapacity(product, containerSize) {
 
 
 
+
 isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)
 isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)
 isEnoughCapacity({ apples: 1, lime: 5, tomatos: 3 }, 14)
